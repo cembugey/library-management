30632cd34042fc5fb798b06bda40011c291ce5aa
Cem Bugey <cembugey@gmail.com>
2025-01-12 10:22:44 +0300

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working');
+});
+
+app.listen(3000, () => {
+  console.log('App working!');
+});
